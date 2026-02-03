e042e0ec020d015937991aa74fec9d827d0b2664
Nishant <nishantrajbhagat123@gmail.com>
2026-02-03 16:29:40 +0530

    new file

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -19,6 +19,6 @@ int main(){
     }
     bubblesort(arr,n);
     for(int i=0; i<n; i++){
-        cout<<arr[i]<<" ";
+        cout<<arr[i]<<"  ";
     }
 }
---
+++ demi.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    n=5;
+    cout<<n;
+    return 0;
+}
